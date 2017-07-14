a478666b3d8e7fb66dddc72a211650794b199559
Felipe <sampaio.motion@gmail.com>
2017-07-14 02:46:08 -0300

    Alteração em manage

--- ProjetoLabBD/settings.py
+++ ProjetoLabBD/settings.py
@@ -116,14 +116,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")

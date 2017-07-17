b44d2fb0dfe44956516e41b113e32b8032bb5471
Felipe <sampaio.motion@gmail.com>
2017-07-17 02:34:40 -0300

    Implementação do index de atendimentos do cuidador

--- prontuarioMedico/urls.py
+++ prontuarioMedico/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^cuidador/(?P<id>\d+)/$', views.cuidador_detalhes, name='cuidador_detalhes'),
     url(r'^cuidador/contratos/$', views.cuidador_contratos, name='cuidador_contratos'),
     url(r'^cuidador/novo/$', views.cuidador_novo, name='cuidador_novo'),
+    url(r'^cuidador/atendimentos/$', views.cuidador_atendimentos, name='cuidador_atendimentos'),
     url(r'^responsabilidades/$', views.responsavel_responsabilidades, name='responsabilidades'),
     url(r'^admin/$', views.admin_index, name='admin')
 ]
--- prontuarioMedico/views.py
+++ prontuarioMedico/views.py
@@ -4,32 +4,34 @@ from prontuarioMedico.data_base import sql_consultas, sql_inserts
 from .forms import CuidadorForm
 from .forms import NameForm
 
+
 def home(request):
-    META_DE_ATENDIMENTOS = 8 # Meta de atendimentos dentro do periodo de um mês
+    META_DE_ATENDIMENTOS = 8  # Meta de atendimentos dentro do periodo de um mês
 
     valores_home = sql_consultas.get_valores_home()
     nro_pacientes = valores_home[0]
-    nro_cuidadores = valores_home[1]
-    nro_responsabilidades = valores_home[2]
+    nro_medicos = valores_home[1]
+    nro_cuidadores = valores_home[2]
+    nro_responsabilidades = valores_home[3]
     qtd_atendimentos_no_ultimo_mes = sql_consultas.get_qtd_atendimento_no_ultimo_mes()
     frac_meta_atendimento = str(int(100 * qtd_atendimentos_no_ultimo_mes / META_DE_ATENDIMENTOS))
 
     context_dictionary = {'pagina': 'home',
                           'nro_pacientes': nro_pacientes,
                           'nro_cuidadores': nro_cuidadores,
-                          'nro_responsabilidades':nro_responsabilidades,
-                          'frac_meta_atendimento':frac_meta_atendimento}
+                          'nro_medicos':nro_medicos,
+                          'nro_responsabilidades': nro_responsabilidades,
+                          'frac_meta_atendimento': frac_meta_atendimento}
 
     return render(request, 'prontuarioMedico/home.html', context_dictionary)
 
 
 # Views de paciente
 def paciente_index(request):
-
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
-            #retorna lista de pacientes que possuem 'nome_paciente' contido no seu nome
+            # retorna lista de pacientes que possuem 'nome_paciente' contido no seu nome
             pacientes_tuple = sql_consultas.get_pacientes_por_nome(form.cleaned_data['nome_paciente'])
         else:
             pacientes_tuple = sql_consultas.get_paciente()
@@ -43,7 +45,7 @@ def paciente_index(request):
                           'datanascimento': paciente_aux[2]})
 
     context_dictionary = {'pagina': 'paciente_index',
-                         'pacientes': pacientes}
+                          'pacientes': pacientes}
     return render(request, 'prontuarioMedico/paciente/paciente_index.html', context_dictionary)
 
 
@@ -73,17 +75,17 @@ def cuidador_contratos(request):
     contratos = []
     for contrato_aux in contratos_tuple:
         contratos.append({'id_contrato': contrato_aux[0],
-               'cpf_cuidador': contrato_aux[1],
-               'nome_cuidador': contrato_aux[2],
-               'cpf_paciente': contrato_aux[3],
-               'nome_paciente': contrato_aux[4],
-               'data_inicio': contrato_aux[5],
-               'data_fim': contrato_aux[6],
-               'tipo_atendimento': contrato_aux[7],
-               'dia_vencimento': contrato_aux[8],
-               'valor_atendimento': contrato_aux[9],
-               'periodicidade': contrato_aux[10]
-               })
+                          'cpf_cuidador': contrato_aux[1],
+                          'nome_cuidador': contrato_aux[2],
+                          'cpf_paciente': contrato_aux[3],
+                          'nome_paciente': contrato_aux[4],
+                          'data_inicio': contrato_aux[5],
+                          'data_fim': contrato_aux[6],
+                          'tipo_atendimento': contrato_aux[7],
+                          'dia_vencimento': contrato_aux[8],
+                          'valor_atendimento': contrato_aux[9],
+                          'periodicidade': contrato_aux[10]
+                          })
     context_dictionary = {'pagina': 'contratos',
                           'contratos': contratos}
     return render(request, 'prontuarioMedico/cuidador/cuidador_contratos.html', context_dictionary)
@@ -103,6 +105,7 @@ def cuidador_novo(request):
     else:
         form = CuidadorForm()
         return render(request, 'prontuarioMedico/cuidador/cuidador_novo.html', {'form': form})
+
 
 def cuidador_detalhes(request, id):
     cuidador_tuple = sql_consultas.get_cuidador_por_cpf(id)
@@ -124,8 +127,26 @@ def cuidador_detalhes(request, id):
     telefones = []
     for aux in telefone_tuple:
         telefones.append({'tipo': aux[0],
-                          'telefone': aux[1] })
-    return render(request, 'prontuarioMedico/cuidador/cuidador_detalhes.html', {'pagina': 'cuidador_detalhes', 'form': cuidador, 'telefone': telefones})
+                          'telefone': aux[1]})
+    return render(request, 'prontuarioMedico/cuidador/cuidador_detalhes.html',
+                  {'pagina': 'cuidador_detalhes', 'form': cuidador, 'telefone': telefones})
+
+
+def cuidador_atendimentos(request):
+    atendimentos_tuple = sql_consultas.get_atendimentos()
+    atendimentos = []
+    for atendimento in atendimentos_tuple:
+        atendimentos.append({'id': atendimento[0],
+                             'horario_inicio': atendimento[1],
+                             'horario_fim': atendimento[2],
+                             'cpf_cuidador': atendimento[3],
+                             'nome_cuidador': atendimento[4],
+                             'cpf_paciente': atendimento[5],
+                             'nome_paciente': atendimento[6]})
+
+    context_dictionary = {'pagina': 'cuidador_atendimentos',
+                          'atendimentos': atendimentos}
+    return render(request, 'prontuarioMedico/cuidador/cuidador_atendimentos.html', context_dictionary)
 
 
 # Views de Responsavel

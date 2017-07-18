a7ab91d5639d44cdd8b5078e7052ac649764af69
italo <it-jorge@hotmail.com>
2017-07-18 04:35:04 -0300

    Chronograma adiciona aos detalhes de cuidador

--- ProjetoLabBD/settings.py
+++ ProjetoLabBD/settings.py
@@ -76,18 +76,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ProntuarioLocal',
-        'USER': 'italo',
-        'PASSWORD': 'asdf',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
